c49be3a3b41c8fcd478349fdc2a9792e03be103b
Balaramreddy <balaramreddy.pandillapalli@gmail.com>
2023-12-18 13:21:03 +0530

    added flutter pages

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 //scroll animation start here
-document.addEventListener("DOMContentLoaded", function() {
-    let navLinks = document.querySelectorAll('.navbar-nav .nav-link');
-    let navbarToggler = document.querySelector('.navbar-toggler');
+// document.addEventListener("DOMContentLoaded", function() {
+//     let navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+//     let navbarToggler = document.querySelector('.navbar-toggler');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
+//     navLinks.forEach(link => {
+//         link.addEventListener('click', function(e) {
+//             e.preventDefault();
 
-            let targetId = this.getAttribute("data-target");
-            let targetSection = document.querySelector(targetId);
+//             let targetId = this.getAttribute("data-target");
+//             let targetSection = document.querySelector(targetId);
 
-            if (targetSection) {
-                targetSection.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'start'
-                });
+//             if (targetSection) {
+//                 targetSection.scrollIntoView({
+//                     behavior: 'smooth',
+//                     block: 'start'
+//                 });
 
-                // Close the navigation menu for small devices
-                if (window.innerWidth < 992) {
-                    navbarToggler.click();
-                }
-            }
-        });
-    });
-});
+//                 // Close the navigation menu for small devices
+//                 if (window.innerWidth < 992) {
+//                     navbarToggler.click();
+//                 }
+//             }
+//         });
+//     });
+// });
 //scroll animation ends here
 
 //pop over started here 
